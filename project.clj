92db0aef0346fe337058fdf9f2d15ed3ea5499ea
Tom Oinn <tomoinn@gmail.com>
2012-03-06 00:35:40 +0000

    Added demo file, a comb-filter based bleeping noise with parameters controlled live with an attached audiocube.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject audiocube-demo "1.0"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [overtone "0.6.0"]
+				 [overtone/overtone.device.audiocubes "0.1.2-SNAPSHOT"]]
+  :dev-dependencies [[lein-eclipse "1.0.0"]])
